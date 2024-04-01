27100abfead371bf6f10d18ce2f80835c77fd7da
igor007-cyber <igor.ramalho007@gmail.com>
2024-04-01 11:20:25 -0300

    nmap e o arquivo principal

--- Dark_night.sh
+++ Dark_night.sh
@@ -32,4 +32,12 @@ echo "#      #";
 echo "#    #"; 
 echo " #";
 sleep 1;
+# verificar o site  
+if [ -z "$1" ]; then
+    echo "Por favor, forneça a URL como argumento."
+    exit 1
+fi
+
+# Extrair o domínio usando sed
+dominio=$(echo "$1" | sed -E 's|^(https?://)?(www\.)?([^/?]+).*|\3|')
 
--- src/ICMP/main.sh
+++ src/ICMP/main.sh
@@ -1,21 +1,32 @@
 # igor
 
-Ping(x){
+ping(x){
     a="$(ping -c 1 -4 x)"; 
     echo $a | grep "PING" | cut -d " " -f3 | tr "()" " " | cut -d " " -f2
     return a
 }
 
-Dig(x){
+dig(x){
     a="$(dig x)"; echo $a | grep "IN" 
     return a
 }
 
-Traceroute(x){
+traceroute(x){
     ips=$(traceroute -n x | awk '{print $2}' | grep -E '([0-9]{1,3}\.){3}[0-9]{1,3}')
     return ips
 }
 
-Nmap(){
-    
+nmap(x){
+    nmap_output=$(nmap -sS -sV "$x")
+
+    # Extrair e imprimir as informações
+    echo "$nmap_output" | awk '/\/tcp/{print "Port: "$1", State: "$2", Service: "$3", Version: "$4}'
+
+}
+
+todos(x){
+    nmap(x)
+    traceroute(x)
+    dig(x)
+    ping(x)
 }
