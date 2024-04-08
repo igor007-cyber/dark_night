bcb3da5ccd1c79476bd88807508fb044f342f4c3
igor007_cyber <igor.ramalho007@gmail.com>
2024-04-08 02:41:17 -0300

    organisando o arquivo subdomain

--- src/Sub-Domain/main.sh
+++ src/Sub-Domain/main.sh
@@ -11,3 +11,19 @@ gobuster_site(){
 
 }
 
+sublist3r_site(){
+
+}
+
+subzy_site(){ # verificação de falha
+
+}
+
+subjack_site(){ # verificação de falha
+
+}
+
+dnsenum_site(){
+    #   dnsenum --dnsserver 8.8.8.8 paodeacucar.com.br
+
+}
