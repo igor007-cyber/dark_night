fdf39cf8fd552a9ea35c632226358568f003bf98
igor007-cyber <igor.ramalho007@gmail.com>
2024-04-12 12:24:55 -0300

    acrescentar no sub domain

--- src/Sub-Domain/main.sh
+++ src/Sub-Domain/main.sh
@@ -5,12 +5,20 @@ export dominio=$(echo "$1" | sed -E 's|^(https?://)?(www\.)?([^/?]+).*|\3|');
 
 # sort -u [arquivo] -o [novo.arquivo] —> ele vai tirar palavras repetidas
 
+# cat newthings.txt | anew things.txt -> Anexe linhas de stdin a um arquivo, mas 
+# somente se elas ainda não aparecerem no arquivo. Também gera novas linhas para 
+# stdout, tornando-o um pouco como um tee -a que remove duplicatas.
+
 dirb_site(){
     #vai verificar diretorios
 }
 
-httprobe_site(){
-    # vai verificar se o site esta ativo
+gobuster_site(){
+    # vai verificar diretorios
+}
+
+knock_site(){
+     # vai verificar os subdominios
 }
 
 assetfinder_site(){
@@ -21,12 +29,13 @@ subfinder_gau(){
     # vai verificar os subdominios
 }
 
-gobuster_site(){
-    # vai verificar diretorios
-}
 
 sublist3r_site(){
     # vai verificar os subdominios
+}
+
+httprobe_site(){
+    # vai verificar se o site esta ativo
 }
 
 subzy_site(){ # verificação de falha
